51e9d0268ddb4c28141a47a9cab14c73b977f220
Lo Zeno <lozeno1982@gmail.com>
2023-11-20 12:24:44 +0000

    implemented 'scent' in grid

---
+++ MartianRobots/MartianRobots/Robot/MarsGrid.cs
@@ -0,0 +1,16 @@
+﻿namespace MartianRobots.Robot;
+
+public class MarsGrid(int width, int height)
+{
+    private readonly bool[,] _grid = new bool[width, height];
+
+    public bool HasScent(int x, int y)
+    {
+        return _grid[x, y];
+    }
+
+    public void MarkCell(int x, int y)
+    {
+        _grid[x, y] = true;
+    }
+}
